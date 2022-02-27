87de4907ef3a9b9841d34c69f9a0d36505018ccf
trisnolase <trisnolase@gmail.com>
2022-02-27 10:10:49 +0700

    export_new_db

--- dbinventarisperalatan.sql
+++ dbinventarisperalatan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2022 at 03:07 PM
+-- Generation Time: Feb 27, 2022 at 04:05 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.0
 
@@ -51,17 +51,16 @@ CREATE TABLE `tblalat` (
 --
 
 INSERT INTO `tblalat` (`id_alat`, `id_kategori`, `id_lokasi`, `nama_peralatan`, `tahun_beli`, `desc_alat`, `jlh_port`, `nama_wifi`, `pass_wifi`, `frek_alat`, `l_frek_alat`, `k_ram`, `k_hardisk`, `t_processor`, `status_alat`, `p_img`) VALUES
-('ALT001', 'KTA001', 'LOK002', 'PC-Diskominfo 001', '2021-12-01', 'Asus PC Core I7 - Office 2012', '-', '-', '-', '-', '-', '8 GB', '1200 GB', 'Core I7', 'Normal', 'FL3pepc.jpg'),
-('ALT003', 'KTA001', 'LOK002', 'PC-Diskominfo 003', '2021-12-20', 'PC', '-', '-', '-', '-', '-', '6 GB', '900 GB', 'I7', 'Rusak Sementara', 'HJLOupc.jpg'),
-('ALT004', 'KTA001', 'LOK005', 'PC-Diskominfo 004', '2021-01-01', 'PC Windows 7 007', '-', '-', '-', '-', '-', '4 GB', '700 GB', 'I7', 'Normal', 'ETqc5pcx.jpg'),
-('ALT009', 'KTA001', 'LOK001', 'PC-Diskominfo 009', '2021-01-01', 'PC Asus + Windows 10 Pro + Office + Anti Virus 2021', '-', '-', '-', '-', '-', '6 GB', '500 GB', 'I5', 'Normal', 'MzoCwpcx.jpg'),
-('ALT012', 'KTA002', 'LOK005', 'MD-Dispen 001', '2021-01-01', 'Modem', '-', '-', '-', '-', '-', '-', '-', '-', 'Rusak Permanen', 'GM1mzmodem.jpg'),
-('ALT033', 'KTA004', 'LOK001', 'PR-Dishub 001', '2021-12-21', 'Epson Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'PSVf6epsprinter.jpg'),
-('ALT036', 'KTA004', 'LOK004', 'PR-Dispen Kota Gusit 001', '2021-11-23', 'Canon Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'NVpx4cprinter.jpg'),
-('ALT074', 'KTA004', 'LOK004', 'PR-Dinas Perikanan 001', '2021-12-21', 'Printer - Scanner', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'dVGrchpprinter.jpg'),
-('ALT099', 'KTA004', 'LOK004', 'PR-Dispen Kota Gusit 002', '2021-12-21', 'HP Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'zkBIShpprinter.jpg'),
-('ALT782', 'KTA002', 'LOK002', 'MD-Dishub', '2017-04-05', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'xg3IZmodem.jpg'),
-('ALT982', 'KAT005', 'LOK001', 'SW-Diskominfo', '2022-01-05', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', '7CZpWswitch.jpg');
+('ALT001', 'KTA001', 'LOK004', 'PC-Diskominfo 001', '2021-12-01', 'Asus PC Core I7 - Office 2012', '-', '-', '-', '-', '-', '8 GB', '1200 GB', 'Core I7', 'Normal', 'FL3pepc.jpg'),
+('ALT003', 'KTA001', 'LOK004', 'PC-Diskominfo 003', '2021-12-20', 'PC', '-', '-', '-', '-', '-', '6 GB', '900 GB', 'I7', 'Rusak Permanen', 'HJLOupc.jpg'),
+('ALT004', 'KTA001', 'LOK001', 'PC-Diskominfo 004', '2021-01-01', 'PC Windows 7', '-', '-', '-', '-', '-', '4 GB', '700 GB', 'I7', 'Normal', 'ETqc5pcx.jpg'),
+('ALT009', 'KTA001', 'LOK001', 'PC-Diskominfo 87', '2021-01-01', 'PC Asus + Windows 10 Pro', '-', '-', '-', '-', '-', '6 GB', '500 GB', 'I5', 'Normal', 'MzoCwpcx.jpg'),
+('ALT033', 'KTA004', 'LOK001', 'PR-Dishub 001', '2021-12-21', 'Epson Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Rusak Sementara', 'PSVf6epsprinter.jpg'),
+('ALT036', 'KTA004', 'LOK004', 'PR-Dispen Kota Gusit 001', '2021-11-23', 'Canon Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Rusak Permanen', 'NVpx4cprinter.jpg'),
+('ALT074', 'KTA004', 'LOK003', 'PR-Dinas Perikanan 001', '2021-12-21', 'Printer - Scanner', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'dVGrchpprinter.jpg'),
+('ALT099', 'KTA004', 'LOK003', 'PR-Dispen Kota Gusit 002', '2021-12-21', 'HP Printer', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'zkBIShpprinter.jpg'),
+('ALT782', 'KTA002', 'LOK002', 'MD-Dishub', '2017-04-05', 'Vodafone Modem', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', 'xg3IZmodem.jpg'),
+('ALT982', 'KAT005', 'LOK001', 'SW-Diskominfo', '2022-01-05', 'D-Link', '-', '-', '-', '-', '-', '-', '-', '-', 'Normal', '7CZpWswitch.jpg');
 
 -- --------------------------------------------------------
 
@@ -92,7 +91,8 @@ INSERT INTO `tblbkat` (`id`, `id_kat`, `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`) V
 (4, 'KTA006', '1', '1', '1', '1', '1', '0', '0', '0'),
 (5, 'KTA002', '1', '1', '1', '1', '1', '0', '0', '0'),
 (6, 'KTA004', '0', '0', '0', '0', '0', '0', '0', '0'),
-(7, 'KTA003', '1', '1', '1', '1', '1', '0', '0', '0');
+(7, 'KTA003', '1', '1', '1', '1', '1', '0', '0', '0'),
+(8, 'KAT663', '0', '0', '0', '0', '0', '0', '0', '0');
 
 -- --------------------------------------------------------
 
@@ -115,14 +115,21 @@ CREATE TABLE `tblgangguan` (
 
 INSERT INTO `tblgangguan` (`id_gangguan`, `id_alat`, `tgl_gangguan`, `ciri`, `deskripsi_gangguan`, `status`) VALUES
 (1, 'ALT001', '2021-12-02', 'Layar berkedip', 'Ketika menjalankan aplikasi Office untuk pertama kali setelah PC diaktifkan layar selalu berkedip.', 'S'),
-(2, 'ALT012', '2021-12-21', '-', 'Tidak terkoneksi ke jaringan internet ketika digunakan', 'S'),
 (3, 'ALT004', '2021-12-21', '-', 'Layar bergaris.', 'S'),
 (4, 'ALT001', '2021-12-21', 'Windows error', 'Windows error dan berbunyi beep', 'S'),
 (5, 'ALT009', '2021-12-21', '-', '-', 'S'),
 (6, 'ALT074', '2021-12-21', '-', 'Hasil cetakan bergaris dan tulisan tidak bisa dibaca', 'S'),
 (7, 'ALT004', '2021-12-30', '-', '-', 'S'),
 (8, 'ALT074', '2021-12-30', 'Tinta putus-putus', 'Hasil print tinta hitam putus-putus dan kabur', 'S'),
-(9, 'ALT003', '2021-12-30', '-', 'Tidak bisa membuka dokumen office word', 'B');
+(9, 'ALT003', '2021-12-30', '-', 'Tidak dapat membuka dokumen office word', 'S'),
+(10, 'ALT036', '2022-02-06', '-', 'Hasil cetakan putus-putus untuk semua warna', 'S'),
+(14, 'ALT004', '2022-02-09', 'Cek', 'Cek', 'S'),
+(15, 'ALT782', '2022-02-09', 'Test', 'Test', 'S'),
+(22, 'ALT001', '2022-02-12', 'Test', 'Ok', 'S'),
+(24, 'ALT003', '2022-02-12', 'Rusak', 'Tidak Hidup', 'S'),
+(25, 'ALT033', '2022-02-12', 'Printer Error', 'Ink absorber full', 'B'),
+(26, 'ALT074', '2022-02-15', '-', '-', 'S'),
+(27, 'ALT001', '2022-02-15', 'Windows error', 'Windows error', 'S');
 
 -- --------------------------------------------------------
 
@@ -149,16 +156,22 @@ INSERT INTO `tblhistorilokasi` (`id_histori`, `id_alat`, `id_lokasi_a`, `id_loka
 (6, 'ALT003', 'LOK001', 'LOK002', '2021-12-21'),
 (7, 'ALT004', 'LOK001', 'LOK002', '2021-12-21'),
 (8, 'ALT009', 'LOK001', 'LOK004', '2021-12-21'),
-(9, 'ALT012', 'LOK002', 'LOK001', '2021-12-21'),
 (11, 'ALT004', 'LOK002', 'LOK003', '2021-12-21'),
 (12, 'ALT004', 'LOK003', 'LOK005', '2021-12-22'),
 (13, 'ALT074', 'LOK005', 'LOK004', '2021-12-21'),
-(17, 'ALT012', 'LOK001', 'LOK004', '2021-12-21'),
-(18, 'ALT012', 'LOK004', 'LOK005', '2021-12-30'),
 (30, 'ALT001', 'LOK004', 'LOK002', '2021-12-31'),
 (33, 'ALT033', 'LOK003', 'LOK001', '2022-01-05'),
 (35, 'ALT009', 'LOK004', 'LOK001', '2022-01-11'),
-(36, 'ALT782', 'LOK003', 'LOK002', '2022-01-11');
+(36, 'ALT782', 'LOK003', 'LOK002', '2022-01-11'),
+(37, 'ALT001', 'LOK002', 'LOK001', '2022-02-09'),
+(38, 'ALT001', 'LOK001', 'LOK005', '2022-02-09'),
+(39, 'ALT003', 'LOK002', 'LOK004', '2022-02-09'),
+(40, 'ALT001', 'LOK005', 'LOK007', '2022-02-09'),
+(41, 'ALT099', 'LOK004', 'LOK003', '2022-02-09'),
+(42, 'ALT001', 'LOK007', 'LOK004', '2022-02-09'),
+(43, 'ALT074', 'LOK004', 'LOK007', '2022-02-12'),
+(44, 'ALT004', 'LOK005', 'LOK001', '2022-02-15'),
+(45, 'ALT074', 'LOK007', 'LOK003', '2022-02-27');
 
 -- --------------------------------------------------------
 
@@ -176,11 +189,12 @@ CREATE TABLE `tblkategori` (
 --
 
 INSERT INTO `tblkategori` (`id_kategori`, `nama_kategori`) VALUES
+('KAT005', 'Switch'),
+('KAT663', 'Monitor'),
 ('KTA001', 'PC'),
 ('KTA002', 'Modem'),
 ('KTA003', 'Router'),
 ('KTA004', 'Printer'),
-('KAT005', 'Switch'),
 ('KTA006', 'HUB');
 
 -- --------------------------------------------------------
@@ -203,7 +217,8 @@ INSERT INTO `tbllokasi` (`id_lokasi`, `nama_lokasi`) VALUES
 ('LOK002', 'Dinas Pendidikan Kota Gunungsitoli'),
 ('LOK003', 'Dishub Kota Gunungsitoli'),
 ('LOK004', 'Dinas Sosial Kota Gunungsitoli'),
-('LOK005', 'Dinas Perikanan Kota Gunungsitoli');
+('LOK005', 'Dinas Perikanan Kota Gunungsitoli'),
+('LOK007', 'Ditjen Pajak');
 
 -- --------------------------------------------------------
 
@@ -233,7 +248,15 @@ INSERT INTO `tblpenanganan` (`id_penanganan`, `id_gangguan`, `tgl_penanganan`, `
 (5, '2', '2021-12-21', 'Hengki', '-', 'Rusak Pemanen', 'Diganti dengan yang baru'),
 (6, '6', '2021-12-29', '-', '-', 'Normal', 'Ok'),
 (7, '7', '2021-12-30', 'Ray', '-', 'Normal', '-'),
-(0, '8', '2022-01-04', 'Ryan', '-', 'Normal', '-');
+(8, '8', '2022-01-04', 'Ryan', '-', 'Normal', '-'),
+(9, '9', '2022-02-06', 'Andre', 'Instal ulang office', 'Normal', '-'),
+(12, '10', '2022-02-09', 'Zalukhu', 'Ok', 'Rusak Permanen', 'Zalukhu'),
+(21, '15', '2022-02-09', 'Ok', 'Ok', 'Normal', 'Ok'),
+(24, '14', '2022-02-09', 'Ok', 'Ok', 'Normal', 'Ok'),
+(25, '22', '2022-02-12', 'Budi', 'Ok', 'Normal', 'Ok'),
+(26, '24', '2022-02-12', 'Budi', 'Diganti', 'Rusak Permanen', 'Beli pc baru'),
+(27, '27', '2022-02-15', 'Moha', 'Ok', 'Normal', 'Ok'),
+(28, '26', '2022-02-22', 'Ryan', 'Ok', 'Normal', 'Ok');
 
 --
 -- Indexes for dumped tables
@@ -264,6 +287,24 @@ ALTER TABLE `tblhistorilokasi`
   ADD PRIMARY KEY (`id_histori`);
 
 --
+-- Indexes for table `tblkategori`
+--
+ALTER TABLE `tblkategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbllokasi`
+--
+ALTER TABLE `tbllokasi`
+  ADD PRIMARY KEY (`id_lokasi`);
+
+--
+-- Indexes for table `tblpenanganan`
+--
+ALTER TABLE `tblpenanganan`
+  ADD PRIMARY KEY (`id_penanganan`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -271,19 +312,25 @@ ALTER TABLE `tblhistorilokasi`
 -- AUTO_INCREMENT for table `tblbkat`
 --
 ALTER TABLE `tblbkat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tblgangguan`
 --
 ALTER TABLE `tblgangguan`
-  MODIFY `id_gangguan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_gangguan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `tblhistorilokasi`
 --
 ALTER TABLE `tblhistorilokasi`
-  MODIFY `id_histori` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_histori` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `tblpenanganan`
+--
+ALTER TABLE `tblpenanganan`
+  MODIFY `id_penanganan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
